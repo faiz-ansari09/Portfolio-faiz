7e96fbe88f369ff25015544a65846b1ac20c5b2b
Faiz <ansarifaiz0905@gmail.com>
2025-09-28 03:17:59 +0530

    Update CV download link in AboutSection.jsx for accessibility

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -34,7 +34,7 @@ export const AboutSection = () => {
                 Get In Touch
               </a>
               <a
-                href=""
+                href="https://drive.google.com/file/d/1vAW78VGjHSGcsKwr1KWqMbEgi6kPlLzS/view?usp=drivesdk"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
